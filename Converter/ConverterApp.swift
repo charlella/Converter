01d43bca2a80d63ce175feba06ce3cb07709ca7a
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-10 09:46:49 +0100

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by charlene hoareau on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
