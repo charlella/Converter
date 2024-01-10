322d15b06001fe81bfe6091b791e9347d0860670
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-10 10:19:23 +0100

    init

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var celsiusTemp = 0.0
+    @State private var fahrTemp = 0.0
+    @State private var kelvinTemp = 0.0
+    
+    func convertTemperature() {
+            // Convert Celsius to Fahrenheit and Kelvin
+            fahrTemp = (celsiusTemp * 9/5) + 32
+            kelvinTemp = celsiusTemp + 273.15
+            
+            // Convert Fahrenheit to Celsius and Kelvin
+            celsiusTemp = (fahrTemp - 32) * 5/9
+            kelvinTemp = (fahrTemp - 32) * 5/9 + 273.15
+            
+            // Convert Kelvin to Celsius and Fahrenheit
+            celsiusTemp = kelvinTemp - 273.15
+            fahrTemp = (kelvinTemp - 273.15) * 9/5 + 32
+        }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Celsius temperature") {
+                    TextField("Celsius", value: $celsiusTemp, format: .number)
+                }
+                Section("Fahrenheit temperature") {
+                    TextField("Fahrenheit", value: $fahrTemp, format: .number)
+                }
+                Section("Kelvin temperature") {
+                    TextField("Kelvin", value: $kelvinTemp, format: .number)
+                }
+            }
+            Button("Convert", systemImage: "ruler", action: convertTemperature)
+                .padding()
+                .foregroundStyle(.white)
+                .background(.blue)
+                .cornerRadius(10)
+
+            .navigationTitle("Temp Converter")
         }
         .padding()
     }
